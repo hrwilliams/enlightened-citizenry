bbab556b1716d64050d163eba0b3366b2c085625
Halle Williams and Vanessa Palacios Sharma <vanessa@>
2016-11-01 13:33:49 -0700

    Add complete page 1 of committees.

--- app/routes/committees.js
+++ app/routes/committees.js
@@ -5,9 +5,21 @@ import Ember from 'ember';
 export default Ember.Route.extend({
  model: function(params) {
    var key = config.myApiKey;
-   var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key;
+   var url = 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key+ '&per_page=50&page=1';
    return Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON.results;
    });
   }
+
+ // model: function(params) {
+ //     var key = config.myApiKey;
+ //   return new Ember.RSVP.hash({
+ //     page1: Ember.$.getJSON({ url: 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key+ '&per_page=50&page=1'}),
+ //     page2: Ember.$.getJSON({ url: 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key+ '&per_page=50&page=2'}),
+ //     page3: Ember.$.getJSON({ url: 'http://congress.api.sunlightfoundation.com/committees?apikey=' +key+ '&per_page=50&page=3'})
+ //   }).then(function(responseJSON) {
+ //     console.log(responseJSON.results);
+ //       return responseJSON.results
+ //   });
+ // }
 });
